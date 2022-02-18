59ce4b4246b65ef71af0192572a87d297ad4fb3f
tanglinxue <31237473+tanglinxue@users.noreply.github.com>
2022-02-18 17:27:39 +0800

    开发

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import store from './store'
 Vue.config.productionTip = false
 
 let v = new Vue({
-  store,
+  name:'root',
+  store, // 此store的目的是让所有组件都能访问到store对象
   render: h => h(App)
 }).$mount('#app')
 
-console.log(v)
+// 所有组件都能执行的方法 Vue.mixin({beforeCreate}), 拿到store挂载到自己的身上
